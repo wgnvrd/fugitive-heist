5ac94fcde3302890f8a6d773a0b59616d1bc7f4f
Konoha Tomono-Duval <konohatomonoduval@gmail.com>
2023-04-04 22:35:25 -0600

    blaaaaaaaaaa

--- src/game_api.py
+++ src/game_api.py
@@ -1,10 +1,22 @@
 from pymongo import MongoClient
+import json
+
+config_file = open("config.json")
+data = json.load(config_file)
+print(data)
+
+with open('config.json') as f:
+    config = json.load(f)
+        bot_token = config['BOT_TOKEN']
 password = input("password for konohatomonoduval: ")
 client = MongoClient("mongodb+srv://konohatomonoduval:" + password + "@anton-1.gzjzi9c.mongodb.net/test") 
 db = client.get_database('fugitive_heist')
 current_game = db.current_game
 
-# Docu
+# Documents
+# Games
+# Players
+
 class GameAPI():
     def __init__(self):
         self.game_state = {
@@ -22,13 +34,14 @@ class GameAPI():
     def get_game_leader(self):
         return self.game_state.leader
 
-    def set_new_player(self, name, team):
-        new_player = {"name":name, "team":team, }
+    def set_new_player(self, discord_id, team):
+        new_player = {"discord_id":discord_id, "team":team, }
         if team == "guards":
             new_player.update({"captures":0})
         elif team == "fugitives":
-            new_player.update({"gem_held":})
-        current_game.insert_one()
+            new_player.update({"gem_held":[], "has_supplies":True, "is_tagged":False})
+
+        current_game.insert_one(new_player)
         
 
     def create_player(self, name, team):
